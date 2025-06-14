b7923994e06f56dff85fe604891388566d927ccd
TisTos-tass3 <tasnim1177@gmail.com>
2025-06-14 22:05:55 +0100

    deleted:    frontend/emigresto-frontend/.vite/deps/_metadata.json

--- web/emigresto-frontend/postcss.config.js
+++
@@ -1,9 +0,0 @@
-// postcss.config.js
-export default {
-    plugins: {
-      // on utilise désormais le plugin dédié
-      '@tailwindcss/postcss': {},
-      autoprefixer: {},
-    }
-  }
-  
--- web/emigresto-frontend/src/config.js
+++
@@ -1,5 +0,0 @@
-// src/config.js
-
-// L’adresse de ton backend Django en dev.  
-// Si tu veux tester sur mobile ou émulateur, il faudra remplacer 127.0.0.1 par l’IP de ta machine.
-export const API_BASE_URL = 'http://127.0.0.1:8000/api/';
